3c50eda461719c301d407d67d5798e6a2daace5f
yogeswarigopireddy <128404101+yogeswarigopireddy@users.noreply.github.com>
2023-03-20 22:04:07 +0530

    Add files via upload

---
+++ y integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter a number: ");
+scanf("%d", &a);
+printf("Enter another number: ");
+scanf("%d", &b);
+c=a+b;
+printf("The sum of %d and %d is %d", a,b,c);
+getch();
+}
