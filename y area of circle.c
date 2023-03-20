0dba4d070f3ca78360202ccfcc43c9d9a532db72
yogeswarigopireddy <128404101+yogeswarigopireddy@users.noreply.github.com>
2023-03-20 21:45:31 +0530

    Add files via upload

---
+++ y area of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int r, area;
+float pi=3.14;
+printf("Enter the radius value: ");
+scanf("%d",&r);
+area=pi*r*r;
+printf("The area is: %d ", area);
+getch();
+return 0;
+}
+
