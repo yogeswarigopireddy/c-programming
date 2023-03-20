4d1717123804dde10c42c6ec77e151bd57492a2e
yogeswarigopireddy <128404101+yogeswarigopireddy@users.noreply.github.com>
2023-03-20 21:50:01 +0530

    Add files via upload

---
+++ y even or odd .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter a number: ");
+	scanf("%d", &a);
+if(a%2==0)
+{
+	printf("even number");
+}
+else
+{
+	printf("odd number");
+}
+getch();
+}
